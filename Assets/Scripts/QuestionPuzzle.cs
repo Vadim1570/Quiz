eed1321a8872c69139ef36d7aa569c9bbf4d213b
Vadim1570 <khamzin-vadim@yandex.ru>
2021-11-20 19:54:49 +0300

    Upgrade Puzzle. Add magnet on placing right position

--- Assets/Scripts/QuestionPuzzle.cs
+++ Assets/Scripts/QuestionPuzzle.cs
@@ -1,48 +1,72 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class QuestionPuzzle : MonoBehaviour
 {
-    GameObject SelectedPiece;
+    public List<PieceWithProperty> piecesWithProperty = new List<PieceWithProperty>();
+    public GameObject selectedPiece;
+    public PieceWithProperty selectedPieceProp;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var piece in  GameObject.FindGameObjectsWithTag("Puzzle"))
+        {
+            //Запоминаем как расставлены кусочки пазла
+            piecesWithProperty.Add(new PieceWithProperty() { pieceGameObject = piece, rightPosition = piece.transform.position });
+            //Разбросать элементы пазла
+            piece.transform.position = new Vector3(Random.Range(-2.18f, 1.32f), Random.Range(-2.72f, -3.72f));
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Запоминаем выбранный элемент пазла
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if(hit != null && hit.transform != null &&
-                hit.transform.CompareTag("Puzzle"))
+            if(hit.transform != null && hit.transform.CompareTag("Puzzle"))
             {
-                SelectedPiece = hit.transform.gameObject;
+                selectedPiece = hit.transform.gameObject;
             }
         }
 
+        //Забываем о выбранном элементе пазла
         if(Input.GetMouseButtonUp(0))
         {
-            SelectedPiece = null;
+            selectedPiece = null;
         }
 
-        if(SelectedPiece != null)
+        if(selectedPiece != null)
         {
+            selectedPieceProp = piecesWithProperty.FirstOrDefault(pp => pp.pieceGameObject == selectedPiece);
+
+            //Двиагем выбранный элемент пазла
             var mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            SelectedPiece.transform.position = new Vector3(mousePoint.x, mousePoint.y, 0f);
+            selectedPiece.transform.position = new Vector3(mousePoint.x, mousePoint.y, 0f);
+
+            //Магнитим выбранный элемент пазла
+            if(Vector3.Distance(selectedPiece.transform.position, selectedPieceProp.rightPosition) < 0.5f)
+            {
+                selectedPiece.transform.position = selectedPieceProp.rightPosition;
+            }
         }
     }
 
     public void SaveAnswearAndLoadScene(string sceneName)
     {
-        //if(AnwearInput.text.ToLower() == RightAnswear.ToLower() )
-        //    ScoreKeeper.GetScoreKeeper().Score += 1;
+        //Если все кусочки лежат в правильных местах, то прибавляем очко
+        if(piecesWithProperty.Count() == piecesWithProperty.Count(p => p.pieceGameObject.transform.position == p.rightPosition))
+            ScoreKeeper.GetScoreKeeper().Score += 1;
 
         SceneManager.LoadScene(sceneName);
     }
+}
+
+[System.Serializable]
+public class PieceWithProperty {
+    public GameObject pieceGameObject;
+    public Vector3 rightPosition;
 }
